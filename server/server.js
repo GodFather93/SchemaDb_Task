0864464eb60f7f2674c700acf7d5ef40fa978a60
GodFather93 <suryapalsingh.inda@gmail.com>
2018-09-23 17:19:39 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.get("/user/:id", (req, res) => {
     });
 });
 
-app.delete("/todos/:id", (req, res) => {
+app.delete("/user/:id", (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
